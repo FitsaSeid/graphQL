311d87a17a9d7c74b4ab7ba47c709ec8094f0fc7
unknown <ethowtv@gmail.com>
2023-02-21 00:49:48 -0300

    Student added

--- server.js
+++ server.js
@@ -1,53 +1,72 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+
 const typeDefs = gql`
     type Query {
-        Students: [Student]
-        Student(id: String): Student
+        students: [student]
+        student(id: String): student
     }
 
-    type Student {
+    type student {
         id: String,
-        name: String,
+        f_name: String,
         GPA: Float,
-        Department: String
+        department: String
     }
 `
-const students_list = [{
+
+const student_data = [{
     id: "123",
-    first_name: "Fitsum",
-    last_name: "Seid",
-    email: "abc@gmail.com",
-    phone_number: 0911001122,
-    GPA: 3.6,
-    Department: "Computer science",
-    profile_mage: "student image",
-}];
+    f_name: "Fitsum",
+    department: "Computer Science",
+    GPA: 3.6
+},
+{
+    id: "1",
+    f_name: "Abrish",
+    department: "SE",
+    GPA: 3.6
+},
+{
+    id: "12",
+    f_name: "Kidus",
+    department: "Computer Science",
+    GPA: 3.6
+},
+{
+    id: "1234",
+    f_name: "Konjit",
+    department: "Computer Science",
+    GPA: 3.6
+},
 
 
+]
 const resolvers = {
     Query: {
-        Students: () => {
-            return students_list;
+
+        students: () => {
+            return student_data
         },
-        Student: (parent, args, context) => {
-            const student_id = args.id;
-            const student = students_list.find(item => item.id === student_id);
+
+        student: (parent, args, context) => {
+            const stud_id = args.id;
+            const student = student_data.find(item => item.id === stud_id);
 
             return student;
         }
-
-        //Retrieve data filtered by name. if there is multiple record with the same name print all of thus
     }
+
+    //Retrieve data filtered by f_name. if there is a multiple records print thus records
 }
 
 const server = new ApolloServer({
-    //schema
-    //resolver
     typeDefs,
     resolvers
 });
 
+
 server.listen().then(({ url }) => {
-    console.log("Server started at" + url);
+    console.log("Server running at " + url
+    )
 })
